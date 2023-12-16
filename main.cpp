2e3345a3d8918a1517310c281ba6cbfba3dbb99c
queshartes <chandlerhineman@yahoo.com>
2023-12-16 14:59:46 -0600

    first commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+// main.cpp
+#include <vulkan/vulkan.h>
+
+int main() {
+    // Initialize windowing system
+    // Create a window
+
+    // Initialize graphics API
+    // Setup shaders and rendering pipeline
+
+    // Main loop
+    while (/* window is open */) {
+        // Handle events
+        // Render frame
+    }
+
+    // Cleanup
+    return 0;
+}
